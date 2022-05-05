a59d9d99a6f515b60c7de54dfe56efa7d800eeb4
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-05 11:50:03 +0200

    added more routes

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,19 @@ const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
-      </Route>
-    </Routes>
+  <Route path="/" element={<App />}>
+    <Route path="expenses" element={<Expenses />} />
+    <Route path="invoices" element={<Invoices />} />
+    <Route
+      path="*"
+      element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      }
+    />
+  </Route>
+</Routes>
   </BrowserRouter>,
   rootElement
 );
